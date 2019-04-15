9e70cf0cc7415a94638bb195d206af2bc67dbb1b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-15 13:02:42 -0700

    continued working on the exercise

--- ratings.py
+++ ratings.py
@@ -1,4 +1,23 @@
 """Restaurant rating lister."""
 
+# read the ratings from the file
+# store as dictionary
+# sorts the ratings
+# print as full sentences
+
+def restaurant_rating(filename):
+    ratings = {}
+
+    with open(filename) as open_file:
+
+        ratings ={open_file}
+
+    for restaurant, rating in ratings.items():
+        print(f"{restaurant} is rated at {rating}.")
+    
+
+
+
 
 # put your code here
+restaurant_rating("scores.txt")
